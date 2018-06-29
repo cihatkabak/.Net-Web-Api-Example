0c9ca026ddfce30e20dfff82338794d9c7bc4c8e
Cihat Kabak <cihatkabak@gmail.com>
2018-06-29 15:59:32 +0300

    Basic Web Api Project

---
+++ WebApiDers2AttributeBasedRouting/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApiDers2AttributeBasedRouting.Global" Language="C#" %>
---
+++ WebApiDers2AttributeBasedRouting/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiDers2AttributeBasedRouting.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
